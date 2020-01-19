baeb2f3a3de89934ea54cbcd3fecab53cf7b231c
Chun-Min Chang <chun.m.chang@gmail.com>
2020-01-19 10:49:22 -0800

    Hide detail unless enter passcode

---
+++ js/keymatcher.js
@@ -0,0 +1,67 @@
+class Queue {
+  constructor(capacity) {
+    this.queue = new Array(capacity);
+    this.size = 0;
+    this.start = 0;
+  }
+  push(value) {
+    let pos = (this.start + this.size) % this.capacity;
+    if (this.capacity == this.size) {
+      // full
+      console.assert(pos == this.start);
+      this.start = (this.start + 1) % this.capacity;
+    } else {
+      // not full
+      this.size += 1;
+    }
+    this.queue[pos] = value;
+    console.assert(this.size <= this.capacity);
+  }
+  get capacity() {
+    return this.queue.length;
+  }
+  get data() {
+    let values = [];
+    for (let i = 0; i < this.size; i++) {
+      let index = (this.start + i) % this.capacity;
+      values.push(this.queue[index]);
+    }
+    return values;
+  }
+}
+
+class KeyMatcher {
+  constructor(keys, onkeymatch) {
+    this.keys = keys;
+    this.onkeymatch = onkeymatch;
+    this.queue = new Queue(keys.length);
+  }
+  init() {
+    document.addEventListener("keydown", evt => {
+      const key = event.key.toLowerCase();
+      this.onkeydown(key);
+    });
+  }
+  onkeydown(key) {
+    this.queue.push(key);
+    if (isEqualArray(this.queue.data, this.keys)) {
+      this.onkeymatch();
+    }
+  }
+}
+
+function isEqualArray(x, y) {
+  return JSON.stringify(x) == JSON.stringify(y);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const key = "resume";
+  let keyMatcher = new KeyMatcher(key.split(""), () => {
+    console.log("show resume!");
+    let elements = document.getElementsByClassName("passcode");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.display = "block";
+    }
+  });
+  keyMatcher.init();
+});
