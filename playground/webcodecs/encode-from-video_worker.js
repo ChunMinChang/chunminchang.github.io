006e852d61caa7f09227e3d44e53d293b12481d5
Chun-Min Chang <chun.m.chang@gmail.com>
2024-10-01 12:18:29 -0700

    Add test: encode from video

---
+++ playground/webcodecs/encode-from-video_worker.js
@@ -0,0 +1,58 @@
+let encoder;
+let encodeTimes = [];
+let outputTimes = [];
+let firstEncodeTime = null;
+let lastOutputTime = null;
+
+function reset() {
+  encoder = null;
+  encodeTimes.length = 0;
+  outputTimes.length = 0;
+  firstEncodeTime = null;
+  lastOutputTime = null;
+}
+
+self.onmessage = async (event) => {
+  if (event.data.command === "configure") {
+    const { command, codec, avcFormat, realTime, width, height } = event.data;
+    encoder = new VideoEncoder({
+      output: (chunk, metadata) => {
+        const endTime = performance.now();
+        outputTimes.push({ timestamp: chunk.timestamp, time: endTime });
+        lastOutputTime = endTime;
+      },
+      error: (e) => console.error(e),
+    });
+    const config = {
+      codec: codec,
+      width: width,
+      height: height,
+      bitrate: 1000000, // 1 Mbps
+      framerate: 30,
+      latencyMode: realTime ? "realtime" : "quality",
+    };
+
+    // Add H264 specific configuration
+    if (codec.startsWith("avc1")) {
+      config.avc = { format: avcFormat };
+    }
+
+    console.log(config);
+
+    encoder.configure(config);
+  } else if (event.data.command === "encode") {
+    const { command, frame } = event.data;
+    const encodeTime = performance.now();
+    encoder.encode(frame);
+    encodeTimes.push({ timestamp: frame.timestamp, time: encodeTime });
+    frame.close();
+    if (firstEncodeTime === null) {
+      firstEncodeTime = encodeTime;
+    }
+  } else if (event.data.command === "flush") {
+    await encoder.flush();
+    const timeDiff = lastOutputTime - firstEncodeTime;
+    self.postMessage({ command: "result", timeDiff, encodeTimes, outputTimes });
+    reset();
+  }
+};
