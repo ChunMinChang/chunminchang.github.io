86356d952d30d7a39b10fe2f68d1650e9364526e
Chun-Min Chang <chun.m.chang@gmail.com>
2025-03-11 09:08:53 -0700

    Simplify photo-videoframe test

--- playground/webcodecs/webrtc.js
+++ playground/webcodecs/webrtc.js
@@ -7,7 +7,26 @@ function getVideoStream(size) {
   return navigator.mediaDevices.getUserMedia(constraints);
 }
 
+function createVideoWithStream(size) {
+  const video = document.createElement("video");
+  video.autoplay = true;
+  getVideoStream(size).then((stream) => {
+    video.srcObject = stream;
+    video.play();
+  });
+  return video;
+}
+
+async function createStreamingVideo(size) {
+  const video = document.createElement("video");
+  video.autoplay = true;
+  const stream = await getVideoStream(size);
+  video.srcObject = stream;
+  await video.play();
+  return { video, stream };
+}
+
 function takePicture(videoElement, ts) {
-  const timestamp = ts || videoElement.currentTime * 1000000 /* microseconds */;
+  const timestamp = ts || videoElement.currentTime * 1000000; /* microseconds */
   return new VideoFrame(videoElement, { timestamp });
 }
